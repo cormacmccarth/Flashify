e2f28d9bf2f22954d73934467e2aa9379ac5c9a3
cormacmccarth <cormacmccarth@gmail.com>
2021-07-18 09:29:57 +0000

    declare and initialize light variable for toggling between modes

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const createCard = document.getElementsByClassName("create-card")[0];
 const question = document.getElementById("question");
 const answer = document.getElementById("answer");
 let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+let light = document.getElementById("light");
+
