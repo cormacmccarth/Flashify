92f12eff96c7905eaccd789857cfb4121636e2f0
cormacmccarth <cormacmccarth@gmail.com>
2021-07-18 09:27:39 +0000

    declare and initialize flashcard, createCard, question and answer variables

---
+++ script.js
@@ -0,0 +1,5 @@
+const flashcards = document.getElementsByClassName("flashcards")[0];
+const createCard = document.getElementsByClassName("create-card")[0];
+const question = document.getElementById("question");
+const answer = document.getElementById("answer");
+let contentArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
